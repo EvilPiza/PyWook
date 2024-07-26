d296abed4fa081e46c4358d045bfd0733f9131fe
Christian Fillmore <140107536+EvilPiza@users.noreply.github.com>
2024-07-25 20:03:37 -0500

    Update Macro.py

--- Python Macro/Macro.py
+++ Python Macro/Macro.py
@@ -1,17 +1,4 @@
-# This is where the actual macro code goes, you shouldn't have to mess with PyHook
+from PyWook import *
+import pyautogui as pag
 
-from PyHook import *
-
-
-################################
-# VARIABLES
-################################
-
-default_webhook_urlwebhook_url = ""
-
-user_id = 00000000000
-
-image_path = ""
-
-
-message("Hello World!", "red")
+# If this is your first time using PyWook, call the function 'help'.
