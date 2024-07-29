2e787385541c786a0ad919ce39226fb40f94cb69
Christian Fillmore <140107536+EvilPiza@users.noreply.github.com>
2024-07-29 15:46:01 -0500

    Update Bot.py

--- Python Macro/Bot.py
+++ Python Macro/Bot.py
@@ -1,12 +1,11 @@
 from discord import Intents, Client, Message, File
 from Bot_responses import get_response
-from PyWook import image_path as IMG_PATH, discord_bot_token as TOKEN
+import PyWook
 import pyscreeze as pys
 
 intents: Intents = Intents.default()
 intents.message_content = True
 client: Client = Client(intents=intents)
-
 
 async def send_message(message_: Message, user_message: str) -> None:
     if not user_message:
@@ -15,13 +14,24 @@ async def send_message(message_: Message, user_message: str) -> None:
 
     if is_private := user_message[0] == '?':
         user_message = user_message[1:]
+
     lowered = user_message.lower()
-    if 'image' in lowered:
+
+    if 'exit' in lowered:
         try:
-            pys.screenshot(IMG_PATH)
-            await message_.author.send(file=File(fp=IMG_PATH)) if is_private else await message_.channel.send(file=File(fp=IMG_PATH))
+            await message_.author.send("Ending Macro...") if is_private else await message_.channel.send("Ending Macro...")
         except Exception as e:
             print(e)
+        PyWook.END()
+        exit()      # This throws an error but still works so idrc
+
+    if 'image' in lowered:
+        try:
+            pys.screenshot(PyWook.image_path)
+            await message_.author.send(file=File(fp=PyWook.image_path)) if is_private else await message_.channel.send(file=File(fp=PyWook.image_path))
+        except Exception as e:
+            print(e)
+
     if user_message[0] == "$":
         user_message = user_message[1:]
         try:
@@ -49,7 +59,7 @@ async def on_message(message: Message) -> None:
     await send_message(message, user_message)
 
 def main() -> None:
-    client.run(token=TOKEN)
+    client.run(token=PyWook.discord_bot_token)
 
 
 if __name__ == '__main__':
