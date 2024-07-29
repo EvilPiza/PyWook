55bd817eb7d97ac515fb1f84bd481b077a32cdf5
Christian Fillmore <140107536+EvilPiza@users.noreply.github.com>
2024-07-29 15:46:53 -0500

    Update Macro.py

--- Python Macro/Macro.py
+++ Python Macro/Macro.py
@@ -1,4 +1,6 @@
-from PyWook import *
+from PyWook import PyWook as pyw, ROBLOX_START_UP as start
 import pyautogui as pag
+from time import sleep
 
-# If this is your first time using PyWook, call the function 'help'.
+
+start()
