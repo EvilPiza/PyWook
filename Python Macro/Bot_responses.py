4e4b0d17c7f62e1eb681c8349602b7429a2e6037
Christian Fillmore <140107536+EvilPiza@users.noreply.github.com>
2024-07-31 12:59:43 -0500

    Update Bot_responses.py
    whoops skipped 1.1, it was never released because I went back and changed tons of stuff so, 1.1 DID exist but never on github

--- Python Macro/Bot_responses.py
+++ Python Macro/Bot_responses.py
@@ -6,17 +6,17 @@ def get_response(user_input: str) -> str:
     if lowered == '':
         return 'Well, you\'re awfully silent...'
     elif 'commands' in lowered:
-        return 'I can tell you about me with "who", you can roll dice with "roll dice" and I can send images to you with "image"!'
+        return 'I can tell you about me with "$who", you can roll dice with "$roll dice" and I can send images to you with "$image"!'
     elif 'roll dice' in lowered:
         return f'You rolled: {randint(1, 6)}'
-    elif 'image' in lowered:
-        return "Alright."
+    elif 'image' in lowered or 'start' in lowered or 'exit' in lowered:
+        return ""
     elif 'version' in lowered:
-        return "I am version 1.0!"
+        return "I am version 1.2!"
     elif 'who' in lowered:
         return """
         I am Py-Bot, a discord bot designed by EvilPiza on GitHub. My main purpose is to send images to you while your away from your PC!
-        If I do anything in correctly, make sure you setup PyWook and if you still are having issues contact me on GitHub or Discord!
+        If I do anything incorrectly, PyWook is setup and if you're still are having issues contact me on GitHub or Discord! (@Bloxfriend590) (@EvilPiza)
                 """
     else:
         return "I don't know what you just said."
